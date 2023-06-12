4734f91d0a4672bf37dfeb0a907272d72c77a781
huydo <huy.do2@sotatek.com>
2023-06-12 15:10:12 +0700

    import them thu vien

---
+++ src/Components/TableUser/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListUser = (props) => {
+  return (
+    <>
+      List...
+    </>
+  )
+}
+
+export default ListUser
---
+++ src/Page/HomePage/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../Components/Header'
+import TableUser from '../../Components/TableUser'
+const HomePage = () => {
+    return (
+        <div>
+            <Header />
+            <TableUser />
+        </div>
+    )
+}
+
+export default HomePage
